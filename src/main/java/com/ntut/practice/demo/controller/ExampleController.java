00c42075eddc63c14599681b0ebe67e208011b9f
KB <kb19900709@gmail.com>
2018-03-04 19:00:42 +0800

    fixed issue of former merging

--- src/main/java/com/ntut/practice/demo/controller/ExampleController.java
+++ src/main/java/com/ntut/practice/demo/controller/ExampleController.java
@@ -1,6 +1,6 @@
 package com.ntut.practice.demo.controller;
 
-import com.ntut.practice.demo.service.MemberService;
+import com.ntut.practice.demo.service.member.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
