67239787bc6b2afe86638b8fde446378dc95c06f
KB <kb19900709@gmail.com>
2018-06-24 14:32:55 +0800

    modify some settings

--- src/main/java/com/ntut/practice/demo/controller/ErrorHandler.java
+++ src/main/java/com/ntut/practice/demo/controller/ErrorHandler.java
@@ -5,18 +5,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 錯誤控制器
  */
 @ControllerAdvice
 public class ErrorHandler {
-
-    /**
-     * 從 src/main/resources/application.yml 取值, ${} 為 SPEL 表達式。
-     * 大部分情境下配合 @Value 比較多
-     */
-    @Value("${project.page.error}")
-    private String ERROR_PAGE;
 
     /**
      * ModelAndView 在建構的時候帶入邏輯名稱
@@ -26,10 +22,10 @@ public class ErrorHandler {
      * @return
      */
     @ExceptionHandler(Exception.class)
-    public ModelAndView forwardToErrorPage(Exception e) {
-        ModelAndView rs = new ModelAndView(ERROR_PAGE);
-        rs.addObject("errorType", e.getClass().getSimpleName());
-        rs.addObject("errorMessage", e.getMessage());
-        return rs;
+    public Map<String, Object> forwardToErrorPage(Exception e) {
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("errorType", e.getClass().getSimpleName());
+        resultMap.put("errorMessage", e.getMessage());
+        return resultMap;
     }
 }
